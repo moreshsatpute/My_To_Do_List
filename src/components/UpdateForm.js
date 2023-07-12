2b62b6744ea2be2dcb308fe3fb369ebd4a0e3e31
Moresh <moreshsatpute@gmail.com>
2023-07-12 12:01:23 +0530

    create a project

---
+++ src/components/UpdateForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+const UpdateForm = ({ updateData, changeTask, updateTask, cancelUpdate }) => {
+  return(
+    <>
+      {/* Update Task */}
+      <div className="row">
+        <div className="col-xs-4" variant="success">
+          <input 
+            value={ updateData && updateData.title }
+            onChange={ (e) => changeTask(e)}
+            className="form-control form-control-lg"
+          />
+        </div>
+        <div className="col-auto">
+          <button
+            onClick={updateTask}
+            className="btn btn-lg btn-success mr-20"
+          >Update</button>
+          <button
+            onClick={cancelUpdate}
+            className="btn btn-lg btn-warning"
+          >Cancel</button>
+        </div>
+      </div>
+      <br />  
+    </>
+  )
+}
+
+export default UpdateForm;
